24458090c9fd8a79e9616d7cecf170dfa1d1b984
Fernando <ferdelamad@gmail.com>
2018-06-22 15:00:20 -0700

    Donde with the send method

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -2,7 +2,18 @@
 // request that you make so you don't have to.
 
 // Put your parse application keys here!
+
+// $.get( "http://parse.sfm8.hackreactor.com/", function( data ) {
+//   $( ".result" ).html( data );
+//   alert( "Load was performed." );
+// });
+
+$.ajax({
+ type: "GET",
+ url: "http://parse.sfm8.hackreactor.com/",
+ });
+
 $.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('X-Parse-Application-Id', 'PARSE_APP_ID');
-  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'PARSE_API_KEY');
+  jqXHR.setRequestHeader('X-Parse-Application-Id', '72b8e073a4abde10221ce95f38ed1c63bd7f3d6b');
+  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'cf1ce23a61e2a40702c347b7dc1e0af8c28f6c7a');
 });
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,76 @@
 // YOUR CODE HERE:
+// Server: http://parse.sfm8.hackreactor.com/
+// App ID: 72b8e073a4abde10221ce95f38ed1c63bd7f3d6b
+// API Key: cf1ce23a61e2a40702c347b7dc1e0af8c28f6c7a
+
+// $.ajaxSetup({
+//   url: "http://parse.sfm8.hackreactor.com/",
+//   global: false,
+//   type: "POST"
+// });
+
+
+var message = {
+  username: 'shawndrost',
+  text: 'trololo',
+  roomname: '4chan'
+};
+
+
+
+
+let app = {};
+
+//init method
+app.init = () => {}
+
+;
+
+var p = '<p>Testing this</p>';
+
+var dat = {
+  username: 'shawndrost',
+  text: 'trololo',
+  roomname: '4chan'
+};
+
+//http://parse.sfm8.hackreactor.com/chatterbox/classes/messages
+//'hrsf99-chatterbox-client/client/index.html'
+//send method
+
+// app.send = (dat) => {
+//   $.post('//http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',   // url
+//        JSON.stringify(dat), // data to be submit
+//        function(data, status, jqXHR) {// success callback
+//                 $('body').append(p);
+//         });
+// };
+
+app.send = (dat) => {
+
+  $.ajax({
+  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+  type: 'POST',
+  data: JSON.stringify(dat),
+  contentType: 'application/json',
+  success: function (data) {
+  	console.log(data);
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+  }
+});
+
+}
+
+
+
+
+//message constructor
+var Message = (username, text, roomname) => {
+  this.username = username;
+  this.text = text;
+  this.roomname = roomname;
+}
