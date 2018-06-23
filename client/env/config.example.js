7a7e35aa7d6a46ee706d04eedefa5ce93ec1e9f5
samMahani <sam.amirmahani@gmail.com>
2018-06-22 20:45:19 -0700

    completed several tests

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -8,11 +8,6 @@
 //   alert( "Load was performed." );
 // });
 
-$.ajax({
- type: "GET",
- url: "http://parse.sfm8.hackreactor.com/",
- });
-
 $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('X-Parse-Application-Id', '72b8e073a4abde10221ce95f38ed1c63bd7f3d6b');
   jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'cf1ce23a61e2a40702c347b7dc1e0af8c28f6c7a');
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -9,68 +9,126 @@
 //   type: "POST"
 // });
 
-
 var message = {
   username: 'shawndrost',
   text: 'trololo',
   roomname: '4chan'
 };
 
-
-
-
-let app = {};
-
-//init method
-app.init = () => {}
-
-;
-
-var p = '<p>Testing this</p>';
-
-var dat = {
-  username: 'shawndrost',
-  text: 'trololo',
-  roomname: '4chan'
-};
-
-//http://parse.sfm8.hackreactor.com/chatterbox/classes/messages
-//'hrsf99-chatterbox-client/client/index.html'
-//send method
-
-// app.send = (dat) => {
-//   $.post('//http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',   // url
-//        JSON.stringify(dat), // data to be submit
-//        function(data, status, jqXHR) {// success callback
-//                 $('body').append(p);
-//         });
-// };
-
-app.send = (dat) => {
-
-  $.ajax({
-  url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-  type: 'POST',
-  data: JSON.stringify(dat),
-  contentType: 'application/json',
-  success: function (data) {
-  	console.log(data);
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
-
-}
-
-
-
-
+var local = new Date();
+var msg;
 //message constructor
 var Message = (username, text, roomname) => {
   this.username = username;
   this.text = text;
   this.roomname = roomname;
 }
+
+class Apps {
+  constructor(username, text, roomname) {
+    this.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
+    
+  }
+
+  init() {
+    $(document).ready(function() {
+      // setInterval(app.fetch, 1000);
+      app.fetch();
+      setTimeout(
+      function loop() {
+
+        msg.results.forEach( (element) => app.renderMessage(element));
+
+      },500)
+
+    })
+  }
+
+  clearMessages() {
+    $('#chats').empty();
+  }
+  
+  renderMessage(message) {
+
+    message.username = message.username || 'anonymous';
+    $('#chats').append(`
+      <div class="chat">
+        <span class="username">${message.username}</span><br>
+        <span>${message.text}</span>
+      </div>
+    `);
+  }
+
+
+  send(dat) {
+    $.ajax({
+      url: this.server,
+      type: 'POST',
+      data: JSON.stringify(dat),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log(data);
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }
+
+
+
+  fetch() {
+    $.ajax({
+      url: this.server,
+      type: 'GET',
+      data: { 
+              order: '-createdAt', 
+              limit: 50 }, 
+      success: function(resp) {
+      
+      msg = resp;
+      console.log(resp);
+      
+
+      },
+      error: function() {
+
+      }
+    });
+  }
+
+};
+
+
+
+let app = new Apps();
+app.fetch();
+// //init method
+// app.init = () => {};
+
+// var p = '<p>Testing this</p>';
+
+// var dat = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
+
+// //http://parse.sfm8.hackreactor.com/chatterbox/classes/messages
+// //'hrsf99-chatterbox-client/client/index.html'
+// //send method
+
+// // app.send = (dat) => {
+// //   $.post('//http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',   // url
+// //        JSON.stringify(dat), // data to be submit
+// //        function(data, status, jqXHR) {// success callback
+// //                 $('body').append(p);
+// //         });
+// // };
+
+
+
+
+
